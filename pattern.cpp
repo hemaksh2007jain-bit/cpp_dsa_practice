361404a503c1898c95fdfbdf085b48c0800b2131
hemaksh2007jain-bit <hemaksh2007jain@gmail.com>
2026-06-20 10:23:10 +0530

    daiky dsa Q

--- pattern.cpp
+++ pattern.cpp
@@ -4,12 +4,22 @@
 using namespace std;
 
 int main() {
-  int a=1;
-  for(int i=1; i<6; i++){
-    for(int j=0; j<i; j++){
-        cout<<a<<" ";
-        a++;
-    }
-    cout<<endl;
+  int a;
+  cout<<"enter a number: ";
+  cin>>a;
+  int original = a;
+  int reversed = 0;
+
+  while (a > 0) {
+    int r = a % 10;
+    reversed = reversed * 10 + r;
+    a = a / 10;
+  }
+
+  if (reversed == original) {
+    cout<<"palindrome";
+  }
+  else {
+    cout<<"not palindrome";
   }
 }
