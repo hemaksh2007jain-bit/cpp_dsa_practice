2463375ddbb6d9d42114633e1b15c16a7a7bb4a9
hemaksh2007jain-bit <hemaksh2007jain@gmail.com>
2026-06-22 13:54:45 +0530

    dsa Q

--- pattern.cpp
+++ pattern.cpp
@@ -3,24 +3,22 @@
 #include <algorithm> 
 using namespace std;
 
-int kadane(int n, int *array){
+int subsum(int n, int *array){
    int maxsum=INT_MIN;
-   int currsum=0;
-   for(int i=0; i<n; i++){
-      currsum=currsum+array[i];
-      maxsum=max(currsum,maxsum);
-      if(currsum<0){
-         currsum=0;
+    for(int i=0; i<n; i++){
+      int currsum=0;
+      for(int j=i; j<n; j++){
+         currsum=currsum+array[j];
+         maxsum=max(maxsum,currsum);
       }
-   }
-   return maxsum;
-
+    }
+    return maxsum;
 }
 
 int main() {
-   int array[]={-1,-2,-3,-4,5,-6,-7};
+   int array[]={2,-3,6,-5,4,2};
    int n=sizeof(array)/sizeof(int);
-   cout<<kadane(n,array);
+   cout<<subsum(n,array);
 
    return 0;
 }
