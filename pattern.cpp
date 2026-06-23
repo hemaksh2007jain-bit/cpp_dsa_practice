e06d5e5a5b1c5b0cbfd9c45ab2f85c0366be5bbe
hemaksh2007jain-bit <hemaksh2007jain@gmail.com>
2026-06-23 09:50:40 +0530

    dsa Q

--- pattern.cpp
+++ pattern.cpp
@@ -3,22 +3,26 @@
 #include <algorithm> 
 using namespace std;
 
-int subsum(int n, int *array){
-   int maxsum=INT_MIN;
-    for(int i=0; i<n; i++){
-      int currsum=0;
-      for(int j=i; j<n; j++){
-         currsum=currsum+array[j];
-         maxsum=max(maxsum,currsum);
-      }
-    }
-    return maxsum;
-}
 
 int main() {
-   int array[]={2,-3,6,-5,4,2};
+   int array[]={7,1,5,3,6,4};
    int n=sizeof(array)/sizeof(int);
-   cout<<subsum(n,array);
-
+   int buy[n];
+   buy[0]=INT_MAX;
+   for(int i=0; i<n; i++){
+     if(buy[i]>array[i]){
+      buy[i+1]=array[i];
+     }else{
+      buy[i+1]=buy[i];
+     }
+   }
+   int profit=array[0]-buy[0];
+   for(int i=1; i<n; i++){
+      int mini=array[i]-buy[i];
+      if(mini>0){
+          profit=max(profit,mini);
+      }
+   }
+   cout<<profit;
    return 0;
 }
