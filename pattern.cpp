90f822091bf8f3d572212e89ea7382c25a9aa839
hemaksh2007jain-bit <hemaksh2007jain@gmail.com>
2026-06-24 15:28:03 +0530

    dsa Q

--- pattern.cpp
+++ pattern.cpp
@@ -6,14 +6,15 @@ using namespace std;
 
 
 int main() {
-   int array[]={5,4,1,3,2};
+   int array[]={5,4,1,6,3,2,7};
    int n=sizeof(array)/sizeof(int);
    for(int i=0; i<n; i++){
-      for(int j=0; j<n-i-1; j++){
+      for(int j=i; j>=0; j--){
          if(array[j]>array[j+1]){
             swap(array[j],array[j+1]);
          }
       }
+      
    }
 
    for(int i=0; i<n; i++){
